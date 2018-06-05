dfa1a77966ffe2dfbcc36249eb451aaaa50ba16c
EGOIST <0x142857@gmail.com>
2018-06-05 15:10:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue-cli-gui
+
+## Preview
+
+Just an Electron around the `vue ui` command, nothing interesting.
+
+<img src="https://i.loli.net/2018/06/05/5b1636b7ca131.png" alt="preview" width="500">
+
+## Download
+
+First you should have `@vue/cli` installed globally on your machine:
+
+```bash
+npm i -g @vue/cli
+```
+
+Then [download this app](https://github.com/egoist/vue-cli-gui/releases).
+
+## License
+
+MIT &copy; EGOIST
---
+++ app/index.js
@@ -0,0 +1,99 @@
+const path = require('path')
+const fs = require('fs')
+const { app, BrowserWindow, Menu, shell } = require('electron')
+const { spawn } = require('child_process')
+const appMenu = require('./menu')
+const config = require('./config')
+const pkg = require('./package')
+
+require('electron-debug')()
+require('electron-context-menu')()
+
+process.env.PATH = `/usr/local/bin:${process.env.PATH}`
+
+const isDev = typeof process.env.NODE_ENV === 'string'
+  ? (process.env.NODE_ENV === 'development')
+  : require('electron-is-dev')
+
+let mainWindow
+let isQuitting = false
+let cmd
+
+// Set title of the app that will use shown in window titlebar
+app.setName(pkg.productName)
+
+const isAlreadyRunning = app.makeSingleInstance(() => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore()
+    }
+
+    mainWindow.show()
+  }
+})
+
+if (isAlreadyRunning) {
+  app.quit()
+}
+
+function createMainWindow() {
+  const lastWindowState = config.get('lastWindowState')
+
+  const win = new BrowserWindow({
+    title: app.getName(),
+    x: lastWindowState.x,
+    y: lastWindowState.y,
+    width: lastWindowState.width,
+    height: lastWindowState.height,
+    show: false
+  })
+
+  win.webContents.on('new-window', (e, url) => {
+    e.preventDefault()
+    shell.openExternal(url)
+  })
+
+  const RE = /Ready on http:\/\/localhost:(\d+)/
+
+  const port = 8347
+  cmd = spawn('vue', ['ui', '--headless', '--port', port])
+  cmd.stdout.on('data', chunk => {
+    const str = chunk.toString()
+    if (RE.test(str)) {
+      win.loadURL(`http://localhost:${port}/project/select`)
+      win.show()
+    }
+  })
+
+  win.on('close', e => {
+    if (!isQuitting) {
+      e.preventDefault()
+
+      if (process.platform === 'darwin') {
+        app.hide()
+      } else {
+        win.hide()
+      }
+    }
+  })
+
+  return win
+}
+
+app.on('ready', () => {
+  Menu.setApplicationMenu(appMenu)
+  mainWindow = createMainWindow()
+})
+
+app.on('activate', () => {
+  mainWindow.show()
+})
+
+app.on('before-quit', () => {
+  isQuitting = true
+
+  cmd && cmd.kill()
+  if (!mainWindow.isFullScreen()) {
+    config.set('lastWindowState', mainWindow.getBounds())
+  }
+})
